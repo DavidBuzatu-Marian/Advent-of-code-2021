a388c27c1664d3b15887a4908cc19b0cd514ac37
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-12-08 11:09:38 +0000

    Solved day 8 brute-forcefully

--- 8.cpp
+++ 8.cpp
@@ -4,33 +4,6 @@
 
 using namespace std;
 
-pair<vector<string>, vector<string>> read_data() {
-    fstream f("in.txt");
-    vector<string> signals;
-    vector<string> outputs;
-    int out = 0;
-    int cnt = 0;
-    while (!f.eof()) {
-        string x;
-        f >> x;
-        if (x == "|") {
-            out = 1;
-            continue;
-        }
-        if (cnt == 4) {
-            out = 0;
-            cnt = 0;
-        }
-        if (out) {
-            outputs.push_back(x);
-            ++cnt;
-        } else {
-            signals.push_back(x);
-        }
-    }
-    return {signals, outputs};
-}
-
 long find_unique_signal_digs(const vector<string> &outputs) {
     long res = 0;
     unordered_set<int> unique_lengths;
@@ -67,18 +40,70 @@ void set_char_pos(const int &dig, const string &sig,
     }
 }
 
-int dfs(unordered_map<string, int> &bit_to_dig,
-        unordered_map<char, int> &char_to_pos, string &bits) {
-    if (bit_to_dig.find(bits) != bit_to_dig.end()) {
-        return 1;
+long dfs(vector<string> &signals, vector<string> &outputs,
+         unordered_map<string, int> &bit_to_dig,
+         unordered_map<char, int> &char_to_pos, const char &curr) {
+    if (curr == 'h') {
+        // all assigned
+        string bits = "0000000";
+        for (char c : signals[0]) {
+            bits[char_to_pos[c]] = '1';
+        }
+        if (bit_to_dig[bits] != 1) {
+            return -1;
+        }
+        bits = "0000000";
+        for (char c : signals[1]) {
+            bits[char_to_pos[c]] = '1';
+        }
+        if (bit_to_dig[bits] != 7) {
+            return -1;
+        }
+        bits = "0000000";
+        for (char c : signals[2]) {
+            bits[char_to_pos[c]] = '1';
+        }
+        if (bit_to_dig[bits] != 4) {
+            return -1;
+        }
+        for (int i = 3; i < signals.size(); ++i) {
+            bits = "0000000";
+            for (char c : signals[i]) {
+                bits[char_to_pos[c]] = '1';
+            }
+            if (bit_to_dig.find(bits) == bit_to_dig.end()) {
+                return -1;
+            }
+        }
+        // everything fine
+        long res = 0;
+        for (string out : outputs) {
+            bits = "0000000";
+            for (char c : out) {
+                bits[char_to_pos[c]] = '1';
+            }
+            res = (res * 10) + (bit_to_dig[bits]);
+        }
+        return res;
+    } else {
+        for (int i = 0; i < 7; ++i) {
+            char_to_pos[curr] = i;
+            long res = dfs(signals, outputs, bit_to_dig, char_to_pos, curr + 1);
+            if (res != -1) {
+                return res;
+            }
+        }
+        return -1;
     }
 }
 
-long find_output_digits_sum(vector<string> &signals,
-                            const vector<string> &outputs) {
+long find_output_digits_sum(vector<string> &signals, vector<string> &outputs) {
     long sum = 0;
     for (string &sig : signals) {
         sort(begin(sig), end(sig));
+    }
+    for (string &out : outputs) {
+        sort(begin(out), end(out));
     }
     sort(begin(signals), end(signals),
          [](const string &a, const string &b) { return a.size() < b.size(); });
@@ -104,6 +129,7 @@ long find_output_digits_sum(vector<string> &signals,
     unordered_map<string, int> bit_to_dig;
     unordered_map<int, string> dig_to_bit;
     unordered_map<char, int> char_to_pos;
+    bit_to_dig["1110111"] = 0;
     bit_to_dig["0010010"] = 1;
     bit_to_dig["1011101"] = 2;
     bit_to_dig["1011011"] = 3;
@@ -114,6 +140,7 @@ long find_output_digits_sum(vector<string> &signals,
     bit_to_dig["1111111"] = 8;
     bit_to_dig["1111011"] = 9;
 
+    dig_to_bit[0] = "1110111";
     dig_to_bit[1] = "0010010";
     dig_to_bit[2] = "1011101";
     dig_to_bit[3] = "1011011";
@@ -124,36 +151,41 @@ long find_output_digits_sum(vector<string> &signals,
     dig_to_bit[8] = "1111111";
     dig_to_bit[9] = "1111011";
 
-    for (string &sig : signals) {
-        if (sig.size() == 2) {
-            set_char_pos(1, sig, char_to_pos, dig_to_bit);
+    return dfs(signals, outputs, bit_to_dig, char_to_pos, 'a');
+}
+
+long long solve_2() {
+    fstream f("in.txt");
+    vector<string> signals;
+    vector<string> outputs;
+    int out = 0;
+    int cnt = 0;
+    long long res = 0;
+    while (!f.eof()) {
+        string x;
+        f >> x;
+        if (x == "|") {
+            out = 1;
+            continue;
         }
-        if (sig.size() == 3) {
-            set_char_pos(7, sig, char_to_pos, dig_to_bit);
+        if (cnt == 4) {
+            out = 0;
+            cnt = 0;
+            res += find_output_digits_sum(signals, outputs);
+            signals = vector<string>();
+            outputs = vector<string>();
         }
-        if (sig.size() == 4) {
-            set_char_pos(4, sig, char_to_pos, dig_to_bit);
-        }
-        if (sig.size() == 7) {
-            set_char_pos(8, sig, char_to_pos, dig_to_bit);
+        if (out) {
+            outputs.push_back(x);
+            ++cnt;
+        } else {
+            signals.push_back(x);
         }
     }
-
-    for (string &sig : signals) {
-        string bits = "0000000";
-        for (int j = 0; j < sig.size(); ++j) {
-            bits[char_to_pos[sig[j]]] = '1';
-        }
-        if (bit_to_dig.find(bits) == bit_to_dig.end()) {
-            dfs(bit_to_dig, bits, char_to_pos);
-        }
-    }
-
-    return sum;
+    return res;
 }
 
 int main() {
-    pair<vector<string>, vector<string>> data = read_data();
-    cout << find_output_digits_sum(data.first, data.second);
+    cout << solve_2();
     return 0;
 }
